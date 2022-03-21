ad1086d918004bf3fc6228626b5970c00d8400c6
dancj <dan@dorvie.com>
2022-03-21 11:12:12 -0400

    finished tutorial part 2

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Layout from "../components/layout";
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About page">
+      <p>Just a test site for learning. Testing is fun and profitable!</p>
+    </Layout>
+  );
+};
+
+export default AboutPage;
