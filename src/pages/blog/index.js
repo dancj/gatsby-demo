05ee57f8662c3a5392263f972af71f4e48bc5b67
dancj <dan@dorvie.com>
2022-03-28 09:36:06 -0400

    finished part 7, added dynamic images to blog posts

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,32 +1,32 @@
 import * as React from "react";
-import { graphql } from "gatsby";
-import { MDXRenderer } from 'gatsby-plugin-mdx'
-import Layout from "../components/layout";
+import { Link, graphql } from "gatsby";
+import Layout from "../../components/layout";
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="Awesome Blog Posts">
-        {data.allMdx.nodes.map((node) => (
-          <article key={node.id}>
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <Link to={`/blog/${node.slug}`}>
             <h2>{node.frontmatter.title}</h2>
             <p>Posted: {node.frontmatter.date}</p>
-            <MDXRenderer>{node.body}</MDXRenderer>
-          </article>
-        ))}
+          </Link>
+        </article>
+      ))}
     </Layout>
   );
 };
 
 export const query = graphql`
   query {
-    allMdx(sort: {order: DESC, fields: frontmatter___date}) {
+    allMdx(sort: { order: DESC, fields: frontmatter___date }) {
       nodes {
         frontmatter {
           date(formatString: "MMMM D, YYYY")
           title
         }
         id
-        body
+        slug
       }
     }
   }
